cfdce7c9cbd44e65f74c8c516e31f9fb7f80ad45
Fivehours0 <724642027@qq.com>
2019-10-28 01:07:16 +0800

    add_remark

--- 1.双指针multimap/1.双指针multimap.cpp
+++ 1.双指针multimap/1.双指针multimap.cpp
@@ -1,3 +1,11 @@
+/*题目描述:
+ * 给定一个整数数组 nums 和一个目标值 target，请你在该数组中找出和为目标值的那
+ * 两个 整数，并返回他们的数组下标。
+ * 你可以假设每种输入只会对应一个答案。但是，你不能重复利用这个数组中同样的元素
+ * 示例:
+ * 给定 nums = [2, 7, 11, 15], target = 9
+ * 因为 nums[0] + nums[1] = 2 + 7 = 9
+ * 所以返回 [0, 1]*/
 #include<iostream>
 #include<vector>
 #include<algorithm>
@@ -11,19 +19,22 @@ void show(T element) { cout << element << " "; }
 vector<int> sum(vector<int>& nums, int target)
 {
 	vector<int> result;
-	multimap<int, int> items;
+	multimap<int, int> items;//保存排序之前的index和value
 	int count = 0;
-	for(auto i: nums)
+	//插入items之后,数据已经排序
+	for(auto i: nums)//将index和value插入items中
 	{ 
 		items.insert(pair<int, int>(i, count)); 
 		count++; 
 	}
-	auto i = items.begin();
-	auto j = --items.end();
+	auto i = items.begin();//头指针
+	auto j = --items.end();//尾指针
 	while(1)
 	{
+		//如果两数之和大于target,尾指针向左移
 		if((*i).first + (*j).first > target)
 			j--;
+		//如果两数之和小于target,头指针向右移
 		else if((*i).first + (*j).first < target)
 			i++;
 		else
