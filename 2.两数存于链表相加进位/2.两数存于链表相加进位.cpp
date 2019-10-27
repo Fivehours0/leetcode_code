f0807f4f28285e757037fd22c99234aa4683fa75
Fivehours0 <724642027@qq.com>
2019-10-28 01:17:51 +0800

    add_remark

--- 2.两数存于链表相加进位/2.两数存于链表相加进位.cpp
+++ 2.两数存于链表相加进位/2.两数存于链表相加进位.cpp
@@ -1,67 +1,32 @@
+/* 问题描述:
+ * 给出两个 非空 的链表用来表示两个非负的整数。其中，它们各自的位数是按照 逆序 
+ * 方式存储的，并且它们的每个节点只能存储 一位 数字。
+ * 如果，我们将这两个数相加起来，则会返回一个新的链表来表示它们的和。
+ * 您可以假设除了数字 0 之外，这两个数都不会以 0 开头。
+ * 示例：
+ * 输入：(2 -> 4 -> 3) + (5 -> 6 -> 4)
+ * 输出：7 -> 0 -> 8
+ * 原因：342 + 465 = 807*/
 #include<iostream>
+#include"ListNode.hpp"
 using namespace std;
 
-struct ListNode{
-	int val;
-	ListNode* next;
-	ListNode(int val_i):val(val_i), next(nullptr) {}
-};
-
-void CreateList(ListNode* phead)
-{
-	ListNode* p = phead;
-	for(int i=0; i<10; i++)
-	{
-		p->val = i;
-		if(i!=9){
-			ListNode* NewNode = new ListNode(0);
-			NewNode->next = nullptr;
-			p->next = NewNode;
-			p = NewNode;
-		}
-		else
-			p->next = nullptr;
-	}
-}
-
-void PrintList(ListNode* phead){
-	cout << "start print" << endl;
-	ListNode* temp = phead;
-	if(nullptr == phead)
-		cout << "it is a nullptr" << endl;
-	while(temp){
-		cout << temp->val << endl;
-		temp = temp->next;
-	}
-}
-
-void DeleteList(ListNode* phead){
-	ListNode* temp = phead;
-	while(nullptr == phead)
-		cout << "it is empty" << endl;
-	while(temp){
-		temp = temp->next;
-		delete phead;
-		phead = temp;
-	}
-	cout << "delete complete" << endl;
-	delete temp;
-}
-
 ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-	int flag = 0;
+	int flag = 0;//进位标志位
 	int add_value = 0;
 	ListNode* p1 = new ListNode(0);
 	ListNode* head = new ListNode(0);
 	head = p1;
 	while(l1||l2)
 	{
+		//两个链表长度不同时,出现l1空的情况
 		if(l1==nullptr)
 		{
 			add_value = l2->val + flag;
 			 l2 = l2->next;
 
 		}
+		//两个链表长度不同时,出现l2空的情况
 		else if(l2==nullptr)
 		{
 			add_value = l1->val + flag;
@@ -75,14 +40,14 @@ ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
 		}
 		flag = add_value / 10;
 		p1->val = add_value % 10;
-		if(l1||l2) 
+		if(l1||l2) //创建存储结果的链表的节点
 		{
 			ListNode* p2 = new ListNode(0);
 			p1->next = p2;
 			p1 = p2;
 		}
 	}
-	
+	//当l1和l2最后节点相加之后产生进位
 	if (l1 == NULL && l2 == NULL && flag == 1)
 		p1->next = new ListNode(1);
 	return head;
@@ -100,4 +65,5 @@ int main(){
 	PrintList(head);
 	DeleteList(l1);
 	DeleteList(l2);
+	DeleteList(head);
 }
